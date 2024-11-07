e18bb0f0b6a4a22c3112d66dbd4a2768ab72b81f
kyucchoi <kyucchoi@naver.com>
2024-11-07 22:48:52 +0900

    :pencil2: Feat: 퀴즈 성공 화면 구현

--- src/pages/quiz/QuizSuccessPage.vue
+++ src/pages/quiz/QuizSuccessPage.vue
@@ -1,7 +1,74 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Main from '@/components/Main.vue';
+import Button from '@/components/ui/button/Button.vue';
+</script>
 
 <template>
-  <div>퀴즈 성공 화면</div>
+  <Main :padded="false" :bg-gray="false">
+    <div class="success-wrapper">
+      <div class="success-container">
+        <div class="point">10,000P</div>
+        <div class="message-container">
+          <div class="message">축하해요! 금융 퀴즈</div>
+          <div class="message">10,000P 받았어요</div>
+        </div>
+      </div>
+    </div>
+
+    <Button variant="default" class="next-button" size="lg">나가기</Button>
+  </Main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.success-wrapper {
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.success-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+}
+
+.point {
+  color: var(--css-dark-yellow);
+  font-size: 48px;
+}
+
+.message-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+}
+
+.message {
+  color: var(--black);
+  font-size: 24px;
+}
+
+.next-button {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: calc(100% - 40px); /* 좌우 여백 20px씩 */
+  max-width: 560px; /* 600px - 40px */
+  margin-top: 0;
+  margin-bottom: 20px;
+}
+
+@media (min-width: 600px) {
+  .next-button {
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: calc(100% - 40px);
+    border-radius: 20px;
+  }
+}
+</style>
