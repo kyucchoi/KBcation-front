03833980e9599f6e0afd9ab3b1174e91d928ab07
김시완 <rkdlqdh112@naver.com>
2024-11-07 17:32:29 +0900

    :sparkles: Feat:퀴즈 로딩 페이지 구현

--- src/pages/quiz/QuizCountingPage.vue
+++ src/pages/quiz/QuizCountingPage.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>퀴즈 로딩 화면</div>
+  <div
+    class="absolute top-[215px] left-[142px] w-[105px] h-[19px] text-[#6a6e77] text-[16px] font-semibold"
+  >
+    금융 지식을 위해
+  </div>
+
+  <div
+    class="absolute top-[240px] left-[92px] w-[207px] h-[66px] text-[#000000] text-[28px] font-semibold flex flex-col justify-center"
+  >
+    <div class="leading-tight">문제의 정확한 답을</div>
+
+    <span class="text-center leading-tight">찾아주세요</span>
+  </div>
+  <div
+    class="absolute top-[336px] left-[47px] w-[294px] h-[24px] text-[#6a6e77] text-[20px] font-semibold"
+  >
+    5단계까지 성공하면 포인트 10,000P
+  </div>
+  <div
+    class="absolute top-[400px] left-[145px] w-[100px] h-[100px] rounded-full bg-[#f59e0c] flex items-center justify-center"
+  >
+    <div
+      class="absolute top-[18px] left-[39px] w-[21px] h-[64px] text-white text-[40px] font-semibold ballo2-font"
+    >
+      3
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* 추가 스타일이 필요하다면 여기 추가 */
+/* font-family를 ballo2-font라는 클래스에서 지정 */
+.ballo2-font {
+  font-family: 'Ballo2', sans-serif;
+}
+/* line-height 값을 조정하여 두 문장 사이의 간격을 좁힘 */
+.leading-tight {
+  line-height: 1.2; /* 기본값보다 줄 간격을 좁힘 */
+}
+</style>
