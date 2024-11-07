9acca341701a4fe53745b448e9069c9c4f29929c
kyucchoi <kyucchoi@naver.com>
2024-11-07 18:10:43 +0900

    :pencil2: Fix: 퀴즈 로딩 페이지 class 정리

--- src/pages/quiz/QuizCountingPage.vue
+++ src/pages/quiz/QuizCountingPage.vue
@@ -1,43 +1,80 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Main from '@/components/Main.vue';
+</script>
 
 <template>
-  <div
-    class="absolute top-[215px] left-[142px] w-[105px] h-[19px] text-[#6a6e77] text-[16px] font-semibold"
-  >
-    금융 지식을 위해
-  </div>
+  <Main :padded="false" :bg-gray="false">
+    <div class="quiz-wrapper">
+      <div class="quiz-container">
+        <div class="guide-text">금융 지식을 위해</div>
 
-  <div
-    class="absolute top-[240px] left-[92px] w-[207px] h-[66px] text-[#000000] text-[28px] font-semibold flex flex-col justify-center"
-  >
-    <div class="leading-tight">문제의 정확한 답을</div>
+        <div class="title-container">
+          <div class="title">문제의 정확한 답을</div>
+          <div class="title">찾아주세요</div>
+        </div>
 
-    <span class="text-center leading-tight">찾아주세요</span>
-  </div>
-  <div
-    class="absolute top-[336px] left-[47px] w-[294px] h-[24px] text-[#6a6e77] text-[20px] font-semibold"
-  >
-    5단계까지 성공하면 포인트 10,000P
-  </div>
-  <div
-    class="absolute top-[400px] left-[145px] w-[100px] h-[100px] rounded-full bg-[#f59e0c] flex items-center justify-center"
-  >
-    <div
-      class="absolute top-[18px] left-[39px] w-[21px] h-[64px] text-white text-[40px] font-semibold ballo2-font"
-    >
-      3
+        <div class="reward-text">5단계까지 성공하면 포인트 10,000P</div>
+
+        <div class="circle-container">
+          <div class="number-text">3</div>
+        </div>
+      </div>
     </div>
-  </div>
+  </Main>
 </template>
 
 <style scoped>
-/* 추가 스타일이 필요하다면 여기 추가 */
-/* font-family를 ballo2-font라는 클래스에서 지정 */
-.ballo2-font {
-  font-family: 'Ballo2', sans-serif;
+.quiz-wrapper {
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-/* line-height 값을 조정하여 두 문장 사이의 간격을 좁힘 */
-.leading-tight {
-  line-height: 1.2; /* 기본값보다 줄 간격을 좁힘 */
+
+.quiz-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 36px 5.13%;
+}
+
+.guide-text {
+  color: var(--dark-gray);
+  font-size: 16px;
+  margin-bottom: 16px;
+}
+
+.title-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+}
+
+.title {
+  color: var(--black);
+  font-size: 28px;
+  line-height: 1.2;
+}
+
+.reward-text {
+  color: var(--dark-gray);
+  font-size: 20px;
+  margin-bottom: 40px;
+}
+
+.circle-container {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: var(--css-dark-yellow);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.number-text {
+  color: var(--white);
+  font-size: 40px;
 }
 </style>
