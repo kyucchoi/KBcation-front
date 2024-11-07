41c6706f1d503816d4fac73371cba23e62f29ad6
kyucchoi <kyucchoi@naver.com>
2024-11-07 13:43:10 +0900

    :sparkles: Feat: 초기 페이지 및 라우터 설정 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
+import QuizCountingPage from '@/pages/quiz/QuizCountingPage.vue';
+import QuizPage from '@/pages/quiz/QuizPage.vue';
+import QuizSuccessPage from '@/pages/quiz/QuizSuccessPage.vue';
+import ChatBotPage from '@/pages/ChatBotPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,26 @@ const router = createRouter({
       path: '/',
       name: 'main',
       component: HomePage
+    },
+    {
+      path: '/quiz/counting',
+      name: 'quizCounting',
+      component: QuizCountingPage
+    },
+    {
+      path: '/quiz',
+      name: 'quiz',
+      component: QuizPage
+    },
+    {
+      path: '/quiz/success',
+      name: 'quizSuccess',
+      component: QuizSuccessPage
+    },
+    {
+      path: '/chatBot',
+      name: 'chatBot',
+      component: ChatBotPage
     }
   ]
 });
